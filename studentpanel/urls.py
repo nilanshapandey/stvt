9c2dc1fd0241e222e06a7ba4bec3437c57052daa
lucifer01430 <harshpandeylucifer@gmail.com>
2025-08-26 23:44:00 +0530

    Admin Panel updated

--- studentpanel/urls.py
+++ studentpanel/urls.py
@@ -20,9 +20,11 @@ urlpatterns = [
     path('admit-card/', views.admit_card, name='admit_card'),
 
     # View Certificate
-    path('certificate/', views.certificate, name='certificate'),
-    path('admin/studentpanel/certificates/view_all/', views.view_all_certificates, name='view_all_certificates'),
-    path("certificates/download-selected/", views.download_selected_certificates, name="download_selected_certificates"),
+    path("certificate/", views.certificate, name="certificate"),   # user panel
+    path("certificate/<int:cert_id>/", views.certificate_admin, name="certificate_admin"),
+    path("certificate/download_selected/", views.download_selected_certificates, name="download_selected_certificates"),
+    path("certificate/view_all/", views.view_all_certificates, name="view_all_certificates"),
+
 
     # Challan view
     path('challan/', views.challan_view, name='challan'),
